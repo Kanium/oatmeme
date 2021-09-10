c4d7a69ece9f632741d00f00b31b23c91ab74390
Fadi Atamny <fadiatamny@gmail.com>
2021-09-10 20:34:01 +0300

    basic structure of meme service

---
+++ butter/src/models/Meme.model.ts
@@ -0,0 +1,53 @@
+import { Dictionary } from 'lodash'
+import { Document, ObjectId } from 'mongodb'
+import Validate from '../utils/Validate'
+
+export interface MemeDocument extends Document {
+    _id: ObjectId
+    name: string
+    data: string
+    updoots: number
+    downdoots: number
+    creatorId: ObjectId
+    createdAt: Date
+    updatedAt: Date
+}
+
+export class Meme implements MemeDocument {
+    public static fromJson(dict: Dictionary<string>) {
+        return new Meme(dict)
+    }
+
+    public _id: ObjectId
+    public name: string
+    public data: string
+    public updoots: number
+    public downdoots: number
+    public creatorId: ObjectId
+    public createdAt: Date
+    public updatedAt: Date
+
+    constructor(meme: Partial<Meme>) {
+        this._id = Validate.ObjectId(meme._id, 'meme._id', true)
+        this.name = Validate.String(meme.name, 'meme.name')
+        this.data = Validate.String(meme.data, 'meme.data')
+        this.updoots = meme.updoots ?? 0
+        this.downdoots = meme.downdoots ?? 0
+        this.creatorId = Validate.ObjectId(meme.creatorId, 'meme.creatorId')
+        this.createdAt = meme.createdAt ?? new Date()
+        this.updatedAt = meme.updatedAt ?? this.createdAt
+    }
+
+    public toJson() {
+        return {
+            _id: this._id.toHexString(),
+            name: this.name,
+            data: this.data,
+            updoots: this.updoots,
+            downdoots: this.downdoots,
+            creatorId: this.creatorId.toHexString(),
+            createdAt: this.createdAt,
+            updatedAt: this.updatedAt
+        }
+    }
+}
--- butter/src/utils/MongoConnector.ts
+++ butter/src/utils/MongoConnector.ts
@@ -1,26 +1,43 @@
-import { Collection, Db, MongoClient } from 'mongodb'
+import { Db, MongoClient, Document, Collection } from 'mongodb'
 
-export default class MongoConnector<T extends Document> {
+export default class MongoConnector {
+    private static _instance: MongoConnector
+    public static get instance() {
+        if (!this._instance) {
+            this._instance = new MongoConnector()
+        }
+        return this._instance
+    }
+    public static heartbeatInterval = Number(process.env.HEATLH_CHECK_INTERVAL) ?? 3.6e6 // 1 hour
+
     private _client: MongoClient | null
     private _db: Db | null
-    private _collection: Collection<T> | null
     private _promise: Promise<void> | null
+    private _timeOut: NodeJS.Timer
 
-    public get collection() {
-        return this._collection
+    public collection<T extends Document>(collectionName: string) {
+        return this._db?.collection<T>(collectionName) as Collection<T>
     }
 
-    constructor(_collectionName: string) {
+    public async healthCheck() {
+        const stats = await this._db?.stats()
+        if (!stats || !stats.ok) {
+            throw Error()
+        }
+        return stats.ok
+    }
+
+    constructor() {
         this._client = null
         this._db = null
-        this._collection = null
-        this._promise = this._initlize(_collectionName)
+        this._promise = this._connect()
+        this._timeOut = setInterval(this.healthCheck, MongoConnector.heartbeatInterval)
     }
 
-    private async _initlize(_collectionName: string) {
+    private async _connect() {
         this._client = await MongoClient.connect('', {})
         this._db = this._client.db()
-        this._collection = this._db.collection(_collectionName)
+        this._promise = null
     }
 
     public async awaitConnection() {
