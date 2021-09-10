b9ec8eef36648f2ee3264df4554830a1aa6f2dbe
Fadi Atamny <fadiatamny@gmail.com>
2021-09-10 21:38:18 +0300

    start of controller

---
+++ butter/src/controllers/Meme.controller.ts
@@ -0,0 +1,12 @@
+import { MemeService } from '../services/Meme.service'
+import { Logger } from '../utils/Logger'
+
+export class MemeController {
+    private _logger: Logger
+    private _service: MemeService
+
+    constructor() {
+        this._logger = new Logger('MemeController')
+        this._service = new MemeService()
+    }
+}
