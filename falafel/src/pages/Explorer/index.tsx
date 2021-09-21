d33a922c14bce85dccd1e0489341842dc391c64c
Fadi Atamny <fadiatamny@gmail.com>
2021-09-21 18:59:10 +0300

    added skeleton of what the component should look like

--- falafel/src/pages/Explorer/index.tsx
+++ falafel/src/pages/Explorer/index.tsx
@@ -1,15 +1,43 @@
 import React from 'react'
+import styles from './styles.module.css'
 import { Helmet } from 'react-helmet'
+import { List, InfiniteLoader, AutoSizer } from 'react-virtualized'
 
 interface ExploreProps {}
 
 const Explore: React.FC = (props: ExploreProps) => {
+    const data = []
+    const size = 10
+    const loadMore = (): Promise<void> => {
+        return new Promise(() => {})
+    }
+    const render = () => (<div></div>)
+
     return (
         <div>
             <Helmet>
                 <title>OatMeme - Explore</title>
             </Helmet>
-            <div>Explore</div>
+            <div>
+                <InfiniteLoader isRowLoaded={() => true} loadMoreRows={loadMore} rowCount={data.length}>
+                    {({ onRowsRendered, registerChild }) => (
+                        <AutoSizer disableHeight>
+                            {({ width }) => (
+                                <List
+                                    ref={registerChild}
+                                    className={styles.List}
+                                    height={200}
+                                    onRowsRendered={onRowsRendered}
+                                    rowCount={data.length}
+                                    rowHeight={30}
+                                    rowRenderer={render}
+                                    width={width}
+                                />
+                            )}
+                        </AutoSizer>
+                    )}
+                </InfiniteLoader>
+            </div>
         </div>
     )
 }
