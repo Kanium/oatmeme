9e7e487c4974e7c38d21b19fef0ed28c9e92a34b
Johan holmgren <ojaohe-3>
2021-09-28 13:55:25 +0200

    inital setup, added virtual scroll packages

---
+++ falafel/src/components/VirtScroll/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import styles from './styles.module.css'
+
+export interface VirtScroll {
+    
+}
+
+// eslint-disable-next-line no-empty-pattern
+const virtScroll = ({}: VirtScroll) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default virtScroll
